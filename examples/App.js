aa37b99f4d385792c2e6908549f06a84fdc85ff4
Christoph Jerolimov <jerolimov+git+2018@gmail.com>
2018-05-01 11:59:15 +0200

    Add example app

---
+++ examples/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import BasicExample from '../packages/rock-native-forms';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  content: {
+    padding: 16,
+  },
+});
+
+export default class App extends Component {
+  render() {
+    return (
+      <KeyboardAwareScrollView style={styles.container} contentContainerStyle={styles.content}>
+        <BasicExample />
+      </KeyboardAwareScrollView>
+    );
+  }
+}
